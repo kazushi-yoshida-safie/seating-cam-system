c02dde0c82c4922610716b884d7c28264442a731
kazushi-yoshida-safie <kazushi.yoshida@safie.jp>
2025-09-29 18:54:16 +0900

    set domain

--- backend/app/src/domain/type.py
+++ backend/app/src/domain/type.py
@@ -1,5 +1,12 @@
 from pydantic import BaseModel
-
+from dataclasses import dataclass
 class RaspiData(BaseModel):
     device_id: str
     encord: str
+
+@dataclass
+class User:
+    """ユーザー情報を保持するデータクラス"""
+    user_id: str
+    name: str
+    face_encoding_str: str # DBから取得した文字列形式の特徴量
