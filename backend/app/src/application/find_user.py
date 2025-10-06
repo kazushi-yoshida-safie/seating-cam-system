6b49836c6b3ff4d073c651dc651ae41745aac166
kazushi-yoshida-safie <kazushi.yoshida@safie.jp>
2025-10-06 11:03:36 +0900

    関数の切り分け

--- backend/app/src/application/find_user.py
+++ backend/app/src/application/find_user.py
@@ -10,28 +10,37 @@ class FindUser:
     アプリケーションのセットアップと実行を担当するクラス
     """
     def __init__(self):
-        self.user_repo = UserRepository() #インスタンス初期化
+        self.user_repo = UserRepository()
         self.comparator = FaceComparator()
 
     def run(self, data:RaspiData):
         print("アプリケーションを実行します...")
-        # print(data.encord)
-        # 3. インスタンスのメソッドを呼び出す
         user_encodings = self.user_repo.get_all_user_face_encodings()        
-        if user_encodings:
+        if not user_encodings:
+             print("not found user_encordings")
+             return
+        #userの特定
+        best_match_user = self.identity_user(user_encodings)
+        
+        print(f"特定したユーザーは{best_match_user}")
+        print("アプリケーションを終了します。")
+    
+    def identify_user(self,data:RaspiData,user_encodings):
+         best_match_user = None
+         min_distance = float('inf')
+        # 3. インスタンスのメソッドを呼び出す
+         if user_encodings:
             print(f"{len(user_encodings)} 件のデータを取得しました。")
-            for i, (user_id, face_encoding) in enumerate(user_encodings[:5]):
-                    print(face_encoding)
-                    result = self.comparator.calculate_distance(data.encord, face_encoding)
-                    print(f"結果は{result}")
-        else:
+            # user特定ロジック
+            for user_id, face_encoding in user_encodings:
+                    result_distance = self.comparator.calculate_distance(data.encord, face_encoding)
+                    if result_distance < min_distance:
+                         min_distance = result_distance
+                         best_match_user = user_id
+            # TODO ここに距離が高すぎる場合は一致ユーザーがいないという処理にする
+         else:
             print("データが見つかりませんでした。")
         
-        print("アプリケーションを終了します。")
+         return best_match_user
+         
 
-# ================== 実行部分 ==================
-# if __name__ == "__main__":
-#     # Applicationクラスをインスタンス化
-#     app = FindUser()
-#     # runメソッドを実行
-# #     app.run()
