fbcb7a29ca5996ff5f2fbae898e504ad9d465988
kazushi-yoshida-safie <kazushi.yoshida@safie.jp>
2025-10-06 18:41:05 +0900

    update

--- backend/app/src/application/find_user.py
+++ backend/app/src/application/find_user.py
@@ -1,10 +1,12 @@
 from interfaces.data_access.user_repository import UserRepository
-import numpy as np
 from domain.type import  User
 from application.comparator import FaceComparator
-# UserRepositoryクラスが定義されているファイルをインポートする必要があります
-# from infrastructure.persistence.user_repository import UserRepository 
+from interfaces.data_access.recording_logs import SeatingRecorder
+from interfaces.slack.slack import Slack
+import numpy as np
+import asyncio
 from domain.type import RaspiData
+
 class FindUser:
     """
     アプリケーションのセットアップと実行を担当するクラス
@@ -12,6 +14,8 @@ class FindUser:
     def __init__(self):
         self.user_repo = UserRepository()
         self.comparator = FaceComparator()
+        self.recorder = SeatingRecorder()
+        self.slack = Slack()
 
     def run(self, data:RaspiData):
         print("アプリケーションを実行します...")
@@ -20,26 +24,32 @@ class FindUser:
              print("not found user_encordings")
              return
         #userの特定
-        best_match_user = self.identity_user(user_encodings)
-        
-        print(f"特定したユーザーは{best_match_user}")
-        print("アプリケーションを終了します。")
+        best_match_user = self.identify_user(data,user_encodings)
+        if best_match_user == None:
+             print("顔識別に失敗しました。")
+             self.recorder.recording_logs(None,data.device_id,False)
+             return
+
+        # ログの追加
+        self.recorder.recording_logs(best_match_user,data.device_id,True)
+        #seatsとdevicesテーブルの更新
+        self.recorder.recording_devices_and_seats_table(data.device_id)
+        #Slackの送信
+        asyncio.run(self.slack.update_slack_status(best_match_user))
+        print("API STATE ALL SUCCESS!!")
     
     def identify_user(self,data:RaspiData,user_encodings):
          best_match_user = None
          min_distance = float('inf')
         # 3. インスタンスのメソッドを呼び出す
-         if user_encodings:
-            print(f"{len(user_encodings)} 件のデータを取得しました。")
+         print(f"{len(user_encodings)} 件のデータを取得しました。")
             # user特定ロジック
-            for user_id, face_encoding in user_encodings:
-                    result_distance = self.comparator.calculate_distance(data.encord, face_encoding)
-                    if result_distance < min_distance:
-                         min_distance = result_distance
-                         best_match_user = user_id
-            # TODO ここに距離が高すぎる場合は一致ユーザーがいないという処理にする
-         else:
-            print("データが見つかりませんでした。")
+         for user_id, face_encoding in user_encodings:
+                result_distance = self.comparator.calculate_distance(data.encord, face_encoding)
+                if result_distance < min_distance:
+                    min_distance = result_distance
+                    best_match_user = user_id
+        # TODO ここに距離が高すぎる場合は一致ユーザーがいないという処理にする
         
          return best_match_user
          
