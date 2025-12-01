83c0f7f821e1b059a028b5b3d800eff958a1648c
kazushi-yoshida-safie <kazushi.yoshida@safie.jp>
2025-12-01 19:01:55 +0900

    /all-seats-infoを追加

--- backend/app/src/interfaces/api/main.py
+++ backend/app/src/interfaces/api/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 import uvicorn
 from application.find_user import FindUser
+from interfaces.data_access.seats_info import SeatsInfo
+
 from domain.type import RaspiData
 # FastAPIアプリケーションのインスタンスを作成
 app = FastAPI()
@@ -11,13 +13,18 @@ app = FastAPI()
 # POSTリクエストを受け取るエンドポイントを定義
 @app.post("/receive-encording")
 def receive_data_from_raspi(data: RaspiData):
-    print(f"  デバイスID: {data.device_id}")
-    print(f"  メッセージ: {data.encord}")
+    print(f"  deviceID: {data.device_id}")
+    print(f"  message: {data.encord}")
     app=FindUser()
     app.run(data)
     
     return {"status": "success", "received_message": data.encord}
 
+@app.get("/all-seats-info/{department_id}")
+def receice_seats_info(department_id: int):
+    app = SeatsInfo()
+    app.res_seats_info(department_id)
+    return {"department_id": department_id}
 # ルートエンドポイント
 @app.get("/")
 def read_root():
