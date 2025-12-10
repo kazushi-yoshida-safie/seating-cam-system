f1db44aa8fc44eec6a14d96836cd255a6096a5f2
kazushi-yoshida-safie <kazushi.yoshida@safie.jp>
2025-12-10 17:50:40 +0900

    add:all-seats-info APIの作成

--- backend/app/src/interfaces/data_access/seats_info.py
+++ backend/app/src/interfaces/data_access/seats_info.py
@@ -1,21 +1,28 @@
 from domain.config import db_config
 import psycopg2
+from psycopg2.extras import RealDictCursor
+from domain.type import Psycopg2Error
 class SeatsInfo:
     def __init__(self):
         self.db_params = db_config.to_dict()
     
     def res_seats_info(self,department_id:int):
-        sql="""SELECT * FROM seats WHERE department_id = %s;"""
+        sql="""SELECT seat_id,is_active,created_at FROM seats WHERE department_id = %s;"""
         try:
             with psycopg2.connect(**self.db_params) as conn:
-                with conn.cursor() as cursor:
+                with conn.cursor(cursor_factory=RealDictCursor) as cursor:
                     cursor.execute(sql,(department_id, ))
                     res = cursor.fetchall()
+                    if not res:
+                        print(f"404: Department ID {department_id} not found.")
+                        raise ValueError(f"Department ID {department_id} not found")
                 print("SUCCSESS : ",res)
+                return res
         except psycopg2.Error as e:
             print(f"database-error : {e}")
             if conn:
                 conn.rollback()
+            raise Psycopg2Error("department not found")
         finally:
             if conn is not None:
                 conn.close()
